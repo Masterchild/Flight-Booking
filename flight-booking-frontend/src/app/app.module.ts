029bfce981b01ca438115a46d14cc57af753a182
Mohammed Ilyas <71893157+Masterchild@users.noreply.github.com>
2023-09-21 01:07:33 +0530

    flight booking frontend

---
+++ flight-booking-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FlightSearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
